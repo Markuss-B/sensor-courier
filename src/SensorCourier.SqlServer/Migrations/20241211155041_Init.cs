985501cbba6cdb22e2473415a73d5f5f9bc98aea
Markuss Birznieks <piebmarkuss@gmail.com>
2024-12-11 17:56:31 +0200

    SqlServer migration.

---
+++ src/SensorCourier.SqlServer/Migrations/20241211155041_Init.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SensorCourier.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parameters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parameters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorMeasurements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SensorId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MetricKey = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MetricValue = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorMeasurements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorMetadata",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SensorId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MetaKey = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MetaValue = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorMetadata", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorMeasurements_SensorId",
+                table: "SensorMeasurements",
+                column: "SensorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorMeasurements_Timestamp",
+                table: "SensorMeasurements",
+                column: "Timestamp");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorMetadata_SensorId",
+                table: "SensorMetadata",
+                column: "SensorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorMetadata_Timestamp",
+                table: "SensorMetadata",
+                column: "Timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parameters");
+
+            migrationBuilder.DropTable(
+                name: "SensorMeasurements");
+
+            migrationBuilder.DropTable(
+                name: "SensorMetadata");
+        }
+    }
+}
